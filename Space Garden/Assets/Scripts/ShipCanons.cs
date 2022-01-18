a3daece40752cc43780681f6ef0d23fdfaa7e5b5
Guillaume Lesage <1998glesage@gmail.com>
2022-01-18 13:44:53 +0100

    faire viser les canon en focntion de la cible de la main

--- Space Garden/Assets/Scripts/ShipCanons.cs
+++ Space Garden/Assets/Scripts/ShipCanons.cs
@@ -24,26 +24,12 @@ public class ShipCanons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateCanonTransform();
+        
 
     }
     void UpdateCanonTransform(){
-        Vector3 canonLeftPosition = canonLeftPositionRef.action.ReadValue<Vector3>();
         
-        Vector3 canonRightPosition = canonRightPositionRef.action.ReadValue<Vector3>();
-        Quaternion canonLeftRotation = canonLeftRotationRef.action.ReadValue<Quaternion>();
-        Quaternion canonRightRotation = canonRightRotationRef.action.ReadValue<Quaternion>();
-
-        Debug.Log(canonLeftPosition.ToString());
-        canonLeftPosition = Quaternion.AngleAxis(-90, Vector3.up) * canonLeftPosition;
-        canonRightPosition = Quaternion.AngleAxis(-90, Vector3.up)* canonRightPosition;
-
-        canonLeft.transform.localPosition=canonLeftPosition + canonLeftBasePosition;
-        Debug.Log(canonLeftPosition.ToString());
-        //canonLeft.transform.localRotation=canonLeftRotation;
-        canonRight.transform.localPosition=canonRightPosition + canonRightBasePosition;
-        //canonRight.transform.localRotation=canonRightRotation;
-
+        
        
 
         
