9b47180e6cb419494b52957f2f8199614715e1c8
Guillaume Lesage <1998glesage@gmail.com>
2022-01-19 14:15:25 +0100

    Aiming Systeme done

--- Space Garden/Assets/Scripts/ShipCanons.cs
+++ Space Garden/Assets/Scripts/ShipCanons.cs
@@ -5,35 +5,42 @@ using UnityEngine.InputSystem;
 
 public class ShipCanons : MonoBehaviour
 {
+    public int range=100;
     public GameObject canonLeft=null;
-    public GameObject canonRight=null;    
-    public InputActionReference canonLeftPositionRef=null;
-    public InputActionReference canonRightPositionRef=null;
-    public InputActionReference canonLeftRotationRef=null;
-    public InputActionReference canonRightRotationRef=null;
-    private Vector3 canonLeftBasePosition;
-    private Vector3 canonRightBasePosition;
+    public GameObject canonRight=null;
+    public GameObject leftHandTarget=null;
+    public GameObject rightHandTarget=null;
+    
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        canonLeftBasePosition=canonLeft.transform.localPosition;
-        canonRightBasePosition=canonRight.transform.localPosition;
+        leftHandTarget.transform.Translate (new Vector3 (0,0,range));
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateCanonLeft();
+        UpdateCanonRight();
+        
+    }
+
+    void UpdateCanonLeft(){
+        canonLeft.transform.LookAt(leftHandTarget.transform);
+        canonLeft.transform.Rotate(new Vector3 (0,90,0));
+
+        //Physics.Raycast(leftHand.transform.position,leftHand.transform.TransformDirection(Vector3.forward), out leftHit, Mathf.Infinity);   
+
+    }
+    
+    void UpdateCanonRight(){
+        canonRight.transform.LookAt(rightHandTarget.transform);
+        canonRight.transform.Rotate(new Vector3 (0,90,0));
         
 
     }
-    void UpdateCanonTransform(){
-        
-        
-       
-
-        
-
-    }
+    
     
 }
